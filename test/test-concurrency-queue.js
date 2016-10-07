a8aae16b652808f8f5f205925f2aaed0636bb0b4
node9 <ash@asiaac.io>
2016-10-07 18:41:35 +0000

    Add tests to ensure FIFO order

--- test/test-concurrency-queue.js
+++ test/test-concurrency-queue.js
@@ -1,6 +1,6 @@
 'use strict';
 var should = require('chai').should();
-var throttledQueue = require('../index.js');
+var concurrencyQueue = require('../index.js');
 
 var job1 = {name: 'job1', 'val' : 4};
 var job2 = {name: 'job2', 'val' : 5};
@@ -36,7 +36,7 @@ describe('Concurrency-Queue', function() {
         });
 
         it("should fire a 'ready' event when adding jobs till maxConcurrency is reached", function() {
-            cQ = throttledQueue.createInstance(options);
+            cQ = concurrencyQueue.createInstance(options);
             cQ.push(job1);
             result.onReady.should.not.equal(undefined);
             result.onReady[0].should.equal(job1);
@@ -51,7 +51,7 @@ describe('Concurrency-Queue', function() {
             result.onReady[2].should.deep.equal({ maxConcurrency: 3, processing: 2, queued: 0 });
         });
         it("should fire a 'queued' event when maxConcurrency reached", function() {
-            cQ = throttledQueue.createInstance(options);
+            cQ = concurrencyQueue.createInstance(options);
             cQ.push(job1);
             cQ.push(job2);
             cQ.push(job3);
@@ -67,7 +67,7 @@ describe('Concurrency-Queue', function() {
             result.onQueued[2].should.deep.equal({ maxConcurrency: 3, processing: 3, queued: 2 });
         });
         it("should fire a 'ready' event and release a queued job when drained", function() {
-            cQ = throttledQueue.createInstance(options);
+            cQ = concurrencyQueue.createInstance(options);
             cQ.push(job1);
             var jobId2 = cQ.push(job2);
             cQ.push(job3);
@@ -81,7 +81,7 @@ describe('Concurrency-Queue', function() {
         });
         it("should fire a 'drained' event and and 'empty' event when completely drained", function() {
             options.maxConcurrency = 2;
-            cQ = throttledQueue.createInstance(options);
+            cQ = concurrencyQueue.createInstance(options);
             var jobId1 = cQ.push(job1);
             var jobId2 = cQ.push(job2);
             cQ.drain(jobId1);
@@ -94,6 +94,27 @@ describe('Concurrency-Queue', function() {
             should.not.exist(result.onEmpty[1]);
             result.onEmpty[2].should.deep.equal({ maxConcurrency: 2, processing: 0, queued: 0 })
         });
+        it("should release entries in the queue following the FIFO rule", function() {
+            options.maxConcurrency = 2;
+            cQ = concurrencyQueue.createInstance(options);
+            var jobId1 = cQ.push(job1);
+            result.onReady[0].should.equal(job1);
+            result.onReady[2].should.deep.equal({ maxConcurrency: 2, processing: 1, queued: 0});
+            var jobId2 = cQ.push(job2);
+            var jobId3 = cQ.push(job3);
+            var jobId4 = cQ.push(job4);
+            result.onQueued[0].should.equal(job4);
+            result.onQueued[2].should.deep.equal({ maxConcurrency: 2, processing: 2, queued: 2});
+            //drain out of order should still maintain input sequence
+            cQ.drain(jobId2);
+            result.onReady[0].should.equal(job3);
+            result.onReady[2].should.deep.equal({ maxConcurrency: 2, processing: 2, queued: 1});
+            cQ.drain(jobId1);
+            result.onReady[0].should.equal(job4);
+            result.onReady[2].should.deep.equal({ maxConcurrency: 2, processing: 2, queued: 0});
+
+        });
+
     });
 });
 
